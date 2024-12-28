04b986ff4559e3ceb832b8ed3abda9729822bc81
Hasunat Shahid <hasunatshahid@gmail.com>
2024-12-28 20:18:10 +0500

    Added comments and improved readability

--- main.cpp
+++ main.cpp
@@ -1,135 +1,111 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
+#include<limits>
 using namespace std;
 
+// Struct to store book information
 struct Book {
     string title;
     string author;
     string ISBN;
-    string yearPublished;
+    int yearPublished;
 };
 
-void addBook(vector<Book>&library) {
-    Book newBook;
-    cout<<"Enter title:";
-    cin>>newBook.title;
-    cout<<"Enter author:";
-    cin>>newBook.author;
-    cout<<"Enter ISBN:";
-    cin>>newBook.ISBN;
-    cout<<"Enter year published:";
-    cin>>newBook.yearPublished;
+// Function to add a new book with year validation
+void addBookWithValidation(vector<Book>& library) {
+    Book newBook;  // Create a new book object
 
-    library.push_back(newBook);
-}
+    // Get book details from the user
+    cout << "Enter title: ";
+    cin.ignore();  
+    getline(cin, newBook.title);  
+    cout << "Enter author: ";
+    getline(cin, newBook.author);  
+    cout << "Enter ISBN: ";
+    cin >> newBook.ISBN;
 
-void  viewBooks(const vector<Book>& library) {
-    for (const Book& book : library) {
-        cout<<"Title:"<<book.title<<endl;
-        cout<<"Author:"<<book.author<<endl;
-        cout<<"ISBN:"<<book.yearPublished<<endl;
-        cout<<"------------------------"<<endl;
-    }
-}
-
-void searchBookByTitle(const vector<Book>&library, const string& title){
-    bool found=false;
-    for(const Book&book:library){
-        if(book.title==title){
-            cout<<"Book found:"<<book.title<<"by"<<book.author<<endl;
-            found=true;
+    // Validate year input
+    cout << "Enter year: ";
+    while (true) {
+        cin >> newBook.yearPublished;
+        if (cin.fail()) {  
+            cin.clear();  
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid year, please enter a valid integer: ";
+        } else {
             break;
         }
     }
-    if(!found){
-        cout<<"No book found with title:"<<title<<endl;
+    
+    // Add the book to the library
+    library.push_back(newBook);
+    cout << "Book added successfully!" << endl;
+}
+
+// Function to display all books in the library
+void viewBooks(const vector<Book>& library) {
+    for (const Book& book : library) {
+        cout << "Title: " << book.title << endl;
+        cout << "Author: " << book.author << endl;
+        cout << "ISBN: " << book.ISBN << endl;
+        cout << "Year Published: " << book.yearPublished << endl;
+        cout << "------------------------" << endl;
     }
 }
 
-void saveBooksToFile(const vector<Book>&library){
-    ofstream file("library.txt");
-    for(const Book&book:library){
-        file<<book.title<<","<<book.author<<","<<book.ISBN<<","<<book.yearPublished<<endl;
+// Function to search for a book by title
+void searchBookByTitle(const vector<Book>& library, const string& title) {
+    bool found = false;
+    for (const Book& book : library) {
+        if (book.title == title) {
+            cout << "Book found: " << book.title << " by " << book.author << endl;
+            found = true;
+            break;
+        }
     }
-    file.close();
-}
-
-void loadBooksFromFile(vector<Book>&library){
-    ifstream file("library.txt");
-    Book book;
-    while(file>>book.title>>book.author>>book.ISBN>>book.yearPublished){
-        library.push_back(book);
+    if (!found) {
+        cout << "No book found with title: " << title << endl;
     }
-    file.close();
 }
-
- void deleteBook(vector<Book>&library,const string& title){
-      for(auto it=library.begin();it!=library.end();++it){
-          if(it->title==title){
-              library.erase(it);
-              cout<<"Book deleted successfully."<<endl;
-              return;
-          }
-      }
-      cout<<"Book not found"<<endl;
-  }
-
-
 
 int main() {
     int choice;
+    vector<Book> library;
 
-    vector<Book>library;
+    while (true) {
+        // Display menu options
+        cout << "Library Management System\n";
+        cout << "1. Add a Book\n";
+        cout << "2. View All Books\n";
+        cout << "3. Search for a Book\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    while(true) {
-        cout<<"Library Management System\n";
-        cout<<"1.Add a Book\n";
-        cout<<"2.View All Books\n";
-        cout<<"3.Search for a Book\n";
-        cout<<"4.Exit\n";
-        cout<<"Enter your choice:";
-        cin>>choice;
-
-        switch(choice){
+        // Perform action based on user choice
+        switch (choice) {
             case 1:
-            cout<<"Add a Book Selected\n";
-            break;
+                addBookWithValidation(library);  
+                break;
             case 2:
-            cout<<"View All Books Selected\n";
-            break;
+                viewBooks(library);  
+                break;
             case 3:
-            cout<<"Search for a Book Selected\n";
-            break;
+                {
+                    string searchTitle;
+                    cout << "Enter title to search: ";
+                    cin >> searchTitle;
+                    searchBookByTitle(library, searchTitle);  
+                }
+                break;
             case 4:
-            cout<<"Exiting program\n";
-            break;
+                cout << "Exiting program\n";
+                return 0;  
             default:
-            cout<<"Invalid choice,try again\n";
-        }
-
-        if(choice=1){
-            addBook(library);
-        }
-        else if(choice==2){
-            viewBooks(library);
-        }
-        else if(choice==3){
-            break;
-        }
-        else{
-            cout<<"Invalid choice, try again."<<endl;
+                cout << "Invalid choice, try again\n";
         }
     }
 
-Book myBook;
-myBook.title="C++ Programming";
-myBook.author="Bjarne Stroustrup";
-myBook.ISBN="123_456789";
-myBook.yearPublished="1985";
-
-cout<<"Title:"<<myBook.title<<endl;
-cout<<"Author:"<<myBook.author<<endl;
-cout<<"ISBN:"<<myBook.ISBN<<endl;
-cout<<"Year Published:"<<myBook.yearPublished<<endl;
+    return 0;  
 }
